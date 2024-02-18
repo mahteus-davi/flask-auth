1b7bd17249c4959d88f03468b268bb0462508f38
Matheus Davi <mahteus2008@gmail.com>
2024-02-18 14:45:36 -0300

    Create route login and logout

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 import bcrypt
 
 
@@ -12,6 +12,41 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:admin123@127.0.0.1
 login_manager = LoginManager()
 db.init_app(app)
 login_manager.init_app(app)
+
+login_manager.login_view = "login"
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and bcrypt.checkpw(str.encode(password), str.encode(user.password)):
+            login_user(user)
+        
+        
+            return jsonify({"message:": "Usuario autenticado com sucesso !"})
+    
+    return jsonify({"message:": "Credenciais inválidas"}), 400
+
+
+@app.route("/logout", methods=["GET"])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "Logout realizado com sucesso!"})
+    
+
 
 @app.route("/user", methods=["POST"])
 def create_user():
